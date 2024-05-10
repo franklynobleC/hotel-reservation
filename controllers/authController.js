0f7a168296b0f9adc4b662deeb7c656c3d49a3b7
franklynobleC <franklyessien01@gmail.com>
2024-05-10 22:31:41 +0100

    add Data for

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const createUserTable = require('../models/User')
 const createRoomsTable = require('../models/Rooms')
 const { StatusCodes } = require('http-status-codes')
 const { dbPool, sql } = require('../db/connectDb')
+var bcrypt = require('bcryptjs')
 
 // const register = async (req, resp) => {
 //   const { email, password, name } = req.body
@@ -13,6 +14,7 @@ const { dbPool, sql } = require('../db/connectDb')
 // }
 //TODO:  HASH password  for  Register,
 const register = async (req, resp) => {
+  // let hashedPassword
   // await createUserTable();
   // await createRoomsTable()
   const { email, password, name } = req.body
@@ -29,28 +31,92 @@ const register = async (req, resp) => {
       .status(StatusCodes.BAD_REQUEST)
       .json(`Enter Email and Password!`)
   }
+  if (password.length <= 5) {
+    console.log(password.length)
+    return resp
+      .status(StatusCodes.BAD_REQUEST)
+      .json(`password length must be at least 6 characters`)
+  } else {
+    //hash  password and  store  in Database
+    bcrypt.genSalt(10, async function (err, salt) {
+      const hashedPassword = bcrypt.hashSync(password, salt)
+      // console.log('hashed Password', hashedPassword)
 
-  try {
-    const users = await sql`
+      try {
+        const users = await sql`
       INSERT INTO users (name, email, password)
-      VALUES (${name}, ${email}, ${password})
+      VALUES (${name}, ${email}, ${hashedPassword})
       RETURNING name, email, password
-    `
+      `
 
-    console.log('from users Data', users)
-    return resp.status(StatusCodes.OK).json(`User added with ID: ${users[0]}`)
-  } catch (error) {
-    console.error(error)
-    return resp.status(StatusCodes.BAD_REQUEST).json({ error: error.message })
+        console.log('from users Data', users)
+
+        return resp
+          .status(StatusCodes.OK)
+          .json(`User added with ID: ${users.columns[1]}`)
+      } catch (error) {
+        console.error(error)
+        return resp
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ error: error.message })
+      }
+    })
   }
 }
 //TODO: VERIFY Password  for  login with  email and  password
 const login = async (req, resp) => {
-  const {} = req.body
-  resp.status(StatusCodes.OK).json({})
+  const { email, password } = req.body
+  console.log(email, password)
+  if (!email || !password) {
+    resp
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: 'please enter email and  password' })
+  }
+  // const user = await sql` SELECT FROM  users  WHERE  email = ${email}`
+  // console.log('from USER Login', user)
+  const user = await sql` SELECT * FROM  users  WHERE  email = ${email}`
+  // .then(data => {
+  //   console.log(data.cursor)
+  // })
+  console.log('from  single User', user)
+
+  // return resp.status(StatusCodes.OK).json(user)
+
+  user.forEach(async data => {
+   const passwordHashed = await data.password
+
+    const  isMatched = await comparePassword(password, passwordHashed)
+    console.log(isMatched, 'from Data compare')
+
+    if (isMatched) {
+
+    }
+  })
+
+  if (user.length === 0) {
+    // console.log(user, 'this user not Found')
+  } else {
+    // console.log('user Data exist ', user.email, user[0])
+    // console.log('user Data exist ', user)
+  }
+}
+
+const comparePassword = async (password, hashedPassword) => {
+  let isMatched
+  await bcrypt.compare(password, hashedPassword).then(res => {
+    if (res === false) {
+      // console.log('false, Not Matched')
+      return (isMatched = false)
+
+      // } else console.log('true')
+    }
+    return (isMatched = true)
+  })
+  return isMatched
 }
 
 module.exports = {
   register,
-  login
+  login,
+  comparePassword
 }
